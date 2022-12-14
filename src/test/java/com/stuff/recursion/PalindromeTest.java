37bd92479aca80cd20e7603e83ce1b339c4e7a29
Pimentel <110205488+Pimentel-J@users.noreply.github.com>
2022-12-14 16:00:01 +0000

    Update PalindromeTest.java

--- src/test/java/com/stuff/recursion/PalindromeTest.java
+++ src/test/java/com/stuff/recursion/PalindromeTest.java
@@ -1,9 +1,5 @@
 package com.stuff.recursion;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,22 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * Tests of class Palindrome.
  */
 class PalindromeTest {
-
-    @BeforeAll
-    static void setUpAll() {
-    }
-
-    @AfterAll
-    static void tearDownAll() {
-    }
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
 
     /**
      * Test of palindrome method, of class Palindrome.
