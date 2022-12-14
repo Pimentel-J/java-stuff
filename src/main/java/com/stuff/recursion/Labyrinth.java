c2810caa7394b2c3f0dd9ecb28f81dbac057e0ff
Pimentel <110205488+Pimentel-J@users.noreply.github.com>
2022-12-14 18:40:05 +0000

    Recursive labyrinth and test

---
+++ src/main/java/com/stuff/recursion/Labyrinth.java
@@ -0,0 +1,79 @@
+package com.stuff.recursion;
+
+/**
+ * Recursive labyrinth
+ *
+ * @author Pimentel
+ */
+public class Labyrinth {
+
+    /**
+     * Checks if there's a way throw the labyrinth
+     *
+     * @param actual the labyrinth in its actual (marked) form
+     * @param y      coordinate y in the labyrinth
+     * @param x      coordinate x in the labyrinth
+     * @return       the marked labyrinth or null if there is no way
+     */
+    public static int[][] check(int[][] actual, int y, int x) {
+        
+        actual[y][x] = 9;
+
+        if (y == actual.length - 1 && x == actual[y].length - 1) {
+            return actual;
+        }
+        //North
+        if (isValid(actual, y - 1, x)) {
+            if (check(actual, y - 1, x) != null) {
+                return actual;
+            }
+        }
+        //East
+        if (isValid(actual, y, x + 1)) {
+            if (check(actual, y, x + 1) != null) {
+                return actual;
+            }
+        }
+        //South
+        if (isValid(actual, y + 1, x)) {
+            if (check(actual, y + 1, x) != null) {
+                return actual;
+            }
+        }
+        //West
+        if (isValid(actual, y, x - 1)) {
+            if (check(actual, y, x - 1) != null) {
+                return actual;
+            }
+        }
+
+        //Backtracking
+        actual[y][x] = 2;
+        return null;
+    }
+
+    /**
+     * Verifies if the next step is a wall (or the labyrinth limit)
+     *
+     * @param actual actual position
+     * @param y      y coordinate
+     * @param x      x coordinate
+     * @return       validation result (boolean)
+     */
+    public static boolean isValid(int[][] actual, int y, int x) {
+        return (y >= 0 && x >= 0 && y < actual.length && x < actual[y].length && actual[y][x] == 1);
+    }
+
+    /**
+     * Prints the matrix
+     *
+     * @param matrix
+     */
+    private static void printMatrix(int[][] matrix) {
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length; y++) {
+                System.out.println(matrix[x][y]);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/stuff/recursion/LabyrinthTest.java
@@ -0,0 +1,60 @@
+package com.stuff.recursion;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests of class Labyrinth.
+ */
+class LabyrinthTest {
+
+    /**
+     * Test of check method, of class Labyrinth.
+     */
+    @Test
+    public void testCheck() {
+        System.out.println("check");
+        int[][] actual = {
+                {1,1,1,0,1,1,0,0,0,1,1,1,1},
+                {1,0,1,1,1,0,1,1,1,1,1,0,1},
+                {1,0,0,0,1,0,1,0,1,0,1,0,1},
+                {1,0,0,0,1,1,1,0,1,0,1,1,1},
+                {1,1,1,1,1,0,0,0,0,1,0,0,0},
+                {0,0,0,0,1,0,0,0,0,0,0,0,0},
+                {0,0,0,0,1,1,1,1,1,1,1,1,1}
+        };
+        int y = 0;
+        int x = 0;
+        int[][] expResult = {
+                {9,9,9,0,2,2,0,0,0,2,2,2,2},
+                {1,0,9,9,9,0,2,2,2,2,2,0,2},
+                {1,0,0,0,9,0,2,0,2,0,2,0,2},
+                {1,0,0,0,9,2,2,0,2,0,2,2,2},
+                {1,1,1,1,9,0,0,0,0,1,0,0,0},
+                {0,0,0,0,9,0,0,0,0,0,0,0,0},
+                {0,0,0,0,9,9,9,9,9,9,9,9,9}
+        };
+        int[][] result = Labyrinth.check(actual, y, x);
+        assertArrayEquals(expResult, result);
+
+        int [] [] impossibleActual = {
+                {1,1,1,0,1,1,0,0,0,1,1,1,1},
+                {1,0,1,1,1,0,1,1,1,1,1,0,1},
+                {1,0,0,0,1,0,1,0,1,0,1,0,1},
+                {1,0,0,0,1,1,1,0,1,0,1,1,1},
+                {1,1,1,1,1,0,0,0,0,1,0,0,0},
+                {0,0,0,0,1,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,1,1,1,1,1,1,1,1}
+        };
+        expResult = null;
+        result = Labyrinth.check(impossibleActual, y, x);
+        assertArrayEquals(expResult, result);
+
+    }
+
+    @Disabled
+    void isValid() {
+    }
+}
