7d02e65ffbdc3006f01519bf8c3afc2c8f3bf4f3
Pimentel <110205488+Pimentel-J@users.noreply.github.com>
2022-12-21 00:35:25 +0000

    Comments update and matrix print

--- src/main/java/com/stuff/recursion/Labyrinth.java
+++ src/main/java/com/stuff/recursion/Labyrinth.java
@@ -2,21 +2,23 @@ package com.stuff.recursion;
 
 /**
  * Recursive labyrinth
- *
- * @author Pimentel
  */
 public class Labyrinth {
 
     /**
      * Checks if there's a way throw the labyrinth
+     * <p>
+     * The right path it marked with 1
+     * The dead-end is marked with 2
+     * The path taken is marked with 9
      *
      * @param actual the labyrinth in its actual (marked) form
      * @param y      coordinate y in the labyrinth
      * @param x      coordinate x in the labyrinth
-     * @return       the marked labyrinth or null if there is no way
+     * @return the marked labyrinth or null if there is no way
      */
     public static int[][] check(int[][] actual, int y, int x) {
-        
+
         actual[y][x] = 9;
 
         if (y == actual.length - 1 && x == actual[y].length - 1) {
@@ -58,7 +60,7 @@ public class Labyrinth {
      * @param actual actual position
      * @param y      y coordinate
      * @param x      x coordinate
-     * @return       validation result (boolean)
+     * @return validation result (boolean)
      */
     public static boolean isValid(int[][] actual, int y, int x) {
         return (y >= 0 && x >= 0 && y < actual.length && x < actual[y].length && actual[y][x] == 1);
@@ -66,14 +68,15 @@ public class Labyrinth {
 
     /**
      * Prints the matrix
-     *
-     * @param matrix
+     * @param matrix labyrinth
      */
     private static void printMatrix(int[][] matrix) {
+        System.out.println("Labyrinth matrix");
         for (int x = 0; x < matrix.length; x++) {
             for (int y = 0; y < matrix[x].length; y++) {
-                System.out.println(matrix[x][y]);
+                System.out.print(matrix[x][y] + " ");
             }
+            System.out.println();
         }
     }
 }
